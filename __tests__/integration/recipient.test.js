102428214372a73505d5cd5e40c14176ab22e67e
Pedro Pizzo de Paula <poloo.p3@gmail.com>
2020-02-20 09:11:25 -0300

    99% of test coverage

--- __tests__/integration/recipient.test.js
+++ __tests__/integration/recipient.test.js
@@ -77,4 +77,79 @@ describe('Recipient', () => {
     expect(response.status).toBe(400);
     expect(response.body.error).toBe('There is no Recipient with id NuN');
   });
+
+  // it('should not be able to register due a validation', async () => {
+  //   const token = await authorize();
+  //   const recipient = await factory.attrs('Recipient');
+
+  //   const recipientRes = await request(app)
+  //     .post('/recipients')
+  //     .set({ Authorization: `Bearer ${token}` })
+  //     .send(recipient);
+
+  //   const response = await request(app)
+  //     .put(`/recipients/${recipientRes.body.id}`)
+  //     .set({ Authorization: `Bearer ${token}` })
+  //     .send({ name: true });
+
+  //   expect(response.status).toBe(400);
+  //   expect(response.body.error).toBe('Validation Fails');
+  // });
+
+  it('should not be able to update a non existing recipient', async () => {
+    const token = await authorize();
+    const response = await request(app)
+      .put(`/recipients/1`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send({});
+
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBe('Recipient not founded');
+  });
+
+  it('should be able to update recipient', async () => {
+    const token = await authorize();
+    const recipient = await factory.attrs('Recipient');
+
+    const recipientRes = await request(app)
+      .post('/recipients')
+      .set({ Authorization: `Bearer ${token}` })
+      .send(recipient);
+
+    const response = await request(app)
+      .put(`/recipients/${recipientRes.body.id}`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send({ name: 'Edited' });
+
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe('Edited');
+  });
+
+  it('should not be able to delete a non existing recipient', async () => {
+    const token = await authorize();
+
+    const response = await request(app)
+      .delete(`/recipients/NuN`)
+      .set({ Authorization: `Bearer ${token}` });
+
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBe('Recipient not founded');
+  });
+
+  it('should be able to delete a recipient', async () => {
+    const token = await authorize();
+    const recipient = await factory.attrs('Recipient');
+
+    const recipientRes = await request(app)
+      .post('/recipients')
+      .set({ Authorization: `Bearer ${token}` })
+      .send(recipient);
+
+    const response = await request(app)
+      .delete(`/recipients/${recipientRes.body.id}`)
+      .set({ Authorization: `Bearer ${token}` });
+
+    expect(response.status).toBe(200);
+    expect(response.body.res).toBe('Recipient is deleted');
+  });
 });
